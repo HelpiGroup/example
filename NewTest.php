5583808022724b92fa09f28a2b0fb7ef1166fdf6
Helpi Group <helpi.ua@gmail.com>
2015-01-04 12:07:51 +0200

    Second Test commit on phpstorm

---
+++ NewTest.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: helpi
+ * Date: 1/4/15
+ * Time: 11:59 AM
+ */
+
+$text = ´test´;
+print_r($text);
---
+++ NewTest2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: helpi
+ * Date: 1/4/15
+ * Time: 12:04 PM
+ */
+
+namespace NewTest;
+
+
+class NewTest2 {
+
+}
